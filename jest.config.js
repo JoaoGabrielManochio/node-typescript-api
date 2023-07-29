37a5f93c70612cec29e01de3dd87c276743b7a20
João Gabriel Manochio <joaogmns123@gmail.com>
2023-07-29 18:05:31 -0300

    adding config

--- jest.config.js
+++ jest.config.js
@@ -4,12 +4,12 @@ const root = resolve(__dirname);
 module.exports = {
     rootDir: root,
     displayName: 'root-tests',
-    testMatch: ['<rootDir>/src/**/*+test+ts'],
+    testMatch: ['<rootDir>/src/**/*.test.ts'],
     testEnvironment: 'node',
     clearMocks: true,
     preset: 'ts-jest',
     moduleNameMapper: {
         '@src/(.*)': '<rootDir>/src/$1',
         '@test/(.*)': '<rootDir>/test/$1',
-    }
-}
+    },
+};
